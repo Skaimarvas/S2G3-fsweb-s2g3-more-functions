7bee9061a9208db849eb7abf60d9e176b262c1be
skaimarvas <skaimarvas@gmail.com>
2023-09-27 19:41:48 +0300

    Tüm sorular cevaplandı. Testten geçildi

--- index.js
+++ index.js
@@ -16,9 +16,18 @@
   örnek output: ""
 */
 
-function dosyaAdiniBul(/* kodlar buraya */) {
-  // kodlar buraya
+function dosyaAdiniBul(path) {
+  const yolBol = path.split("/");
+  const dosya = yolBol[yolBol.length - 1];
+  return dosya;
 }
+
+console.log(
+  "Görev 1:",
+  dosyaAdiniBul(
+    "C:Users/lenovo/Desktop/Workintech-0923/Proje Çözümleri/S2G1-index"
+  )
+);
 
 /*
   GÖREV 2
@@ -37,10 +46,34 @@ function dosyaAdiniBul(/* kodlar buraya */) {
   örnek input:  [109, 216, 288, 143, 71, 185, -278, 194, 5]
   örnek output: 104
 */
+const bosDizi = [];
+const sayiDizi = [109, 216, 288, 143, 71, 185, -278, 194, 5];
+const sayiDizi2 = [50, -26, 153, 7];
 
-function ortalamaBul(/* kodlar buraya */) {
-  // kodlar buraya
+const tekSayi = [4];
+
+function ortalamaBul(arrDizi) {
+  if (arrDizi.length === 0) {
+    return null;
+  }
+  if (arrDizi.length === 1) {
+    return arrDizi[0];
+  }
+  let diziToplam = 0;
+
+  for (let i = 0; i < arrDizi.length; i++) {
+    diziToplam += arrDizi[i];
+  }
+
+  const aritmetikOrtalama = diziToplam / arrDizi.length;
+
+  return aritmetikOrtalama;
 }
+
+console.log("Görev 2:", ortalamaBul(sayiDizi));
+console.log("Görev 2: Bos dizi :", ortalamaBul(bosDizi));
+console.log("Görev 2: Çok sayıda dizi:", ortalamaBul(sayiDizi2));
+console.log("Görev 2: Tek sayı:", ortalamaBul(tekSayi));
 
 /*
   GÖREV 3
@@ -62,9 +95,58 @@ function ortalamaBul(/* kodlar buraya */) {
   örnek output: [109, 216, 288, 143, 185, 194]
 */
 
-function ortalamadanBuyukleriBul(/* kodlar buraya */) {
-  // kodlar buraya
+const bosDizi1 = [];
+const sayiDizi1 = [109, 216, 288, 143, 71, 185, -278, 194];
+const sayiDizi3 = [50, -26, 153, 7];
+
+const tekSayi1 = [5];
+
+// function ortalamadanBuyukleriBul(arrSayi, ortalamaFonksiyonu) {
+//   if (!arrSayi || arrSayi.length === 0) {
+//     return null;
+//   }
+//   const ortalama = ortalamaFonksiyonu(arrSayi);
+//   const buyuk = arrSayi.filter((sayi) => sayi >= ortalama);
+//   return buyuk;
+// }
+
+function ortalamadanBuyukleriBul(diziArr, ortBul) {
+  if (diziArr.length === 0) {
+    return null;
+  }
+  if (diziArr.length === 1) {
+    return diziArr;
+  }
+  let ortBuyukSayilar = [];
+  const ortSayi = ortBul(diziArr);
+
+  for (let i = 0; i < diziArr.length; i++) {
+    if (diziArr[i] >= ortSayi) {
+      ortBuyukSayilar.push(diziArr[i]);
+    }
+  }
+  return ortBuyukSayilar;
 }
+
+console.log(
+  "Görev 3: Boş dizi:",
+  ortalamadanBuyukleriBul(bosDizi1, ortalamaBul)
+);
+
+console.log(
+  "Görev 3: Tek sayı:",
+  ortalamadanBuyukleriBul(tekSayi1, ortalamaBul)
+);
+
+console.log(
+  "Görev 3: Az sayıda dizi:",
+  ortalamadanBuyukleriBul(sayiDizi3, ortalamaBul)
+);
+
+console.log(
+  "Görev 3: Çok sayıda dizi:",
+  ortalamadanBuyukleriBul(sayiDizi1, ortalamaBul)
+);
 
 /* !!!! Burdan aşağısını değiştirmeyin !!!! */
 function as() {
